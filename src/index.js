fdb5df9ede952d605bd28b7eec8ff3034d25ffaa
Francis Maina <sebastianfrancix@gmail.com>
2022-12-21 15:20:58 +0300

    normal functions vs arrow func

--- src/index.js
+++ src/index.js
@@ -5,4 +5,10 @@ function Greeting() {
   return <h4>Greetings from Francis, this is my first component.</h4>;
 }
 
+// const Greeting = () => {
+//   return React.createElement("h1", {}, "Hello World!");
+// };
+
+export default Greeting;
+
 ReactDOM.render(<Greeting />, document.getElementById("root"));
