c54d54ae43a8b9b26eb9bb08212c35305107a211
Francis Maina <sebastianfrancix@gmail.com>
2022-12-22 12:23:13 +0300

    refactored the code to use one component

--- src/index.js
+++ src/index.js
@@ -18,28 +18,18 @@ function BookList() {
 const Book = () => {
   return (
     <article className="book">
-      <Image />
-      <Title />
-      <Author />
+      <img
+        src="https://m.media-amazon.com/images/W/WEBP_402378-T2/images/I/51VC+Vru96L._SY344_BO1,204,203,200_.jpg"
+        alt="BookS"
+      />
+      <h1>It Starts with Us: A Novel (It Ends with Us) </h1>
+      <h4
+        style={{ color: "#617d98", fontSize: "0.75rem", marginTop: "0.25rem" }}
+      >
+        Colleen Hoover
+      </h4>
     </article>
   );
-};
-
-const Image = () => {
-  return (
-    <img
-      src="https://m.media-amazon.com/images/W/WEBP_402378-T2/images/I/51VC+Vru96L._SY344_BO1,204,203,200_.jpg"
-      alt="BookS"
-    />
-  );
-};
-
-const Title = () => {
-  return <h1>It Starts with Us: A Novel (It Ends with Us) </h1>;
-};
-
-const Author = () => {
-  return <h4>Colleen Hoover</h4>;
 };
 
 export default BookList;
