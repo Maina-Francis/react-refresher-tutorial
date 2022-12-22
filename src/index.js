d728cca133161bdd4b21dbb30e77ffb2446f6930
Francis Maina <sebastianfrancix@gmail.com>
2022-12-22 13:02:35 +0300

    intro to props

--- src/index.js
+++ src/index.js
@@ -2,31 +2,47 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
+// setup variables
+const firstBook = {
+  author: "Colleen Hoover",
+  title: "It Starts with Us: A Novel (It Ends with Us)",
+  img: "https://m.media-amazon.com/images/W/WEBP_402378-T2/images/I/51VC+Vru96L._SY344_BO1,204,203,200_.jpg",
+};
+
+const secondBook = {
+  author: "Michelle Obama ",
+  title: "The Light We Carry: Overcoming in Uncertain Times",
+  img: "https://m.media-amazon.com/images/W/WEBP_402378-T2/images/I/41HcM1yzHcL._SY291_BO1,204,203,200_QL40_FMwebp_.jpg",
+};
+
 function BookList() {
   return (
     <section className="booklist">
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
-      <Book />
+      <Book
+        author={firstBook.author}
+        title={firstBook.title}
+        img={firstBook.img}
+      />
+
+      <Book
+        author={secondBook.author}
+        title={secondBook.title}
+        img={secondBook.img}
+      />
     </section>
   );
 }
 
-const Book = () => {
+const Book = (props) => {
+  console.log(props);
   return (
     <article className="book">
-      <img
-        src="https://m.media-amazon.com/images/W/WEBP_402378-T2/images/I/51VC+Vru96L._SY344_BO1,204,203,200_.jpg"
-        alt="BookS"
-      />
-      <h1>It Starts with Us: A Novel (It Ends with Us) </h1>
+      <img src={props.img} alt="" />
+      <h1>{props.title} </h1>
       <h4
         style={{ color: "#617d98", fontSize: "0.75rem", marginTop: "0.25rem" }}
       >
-        Colleen Hoover
+        {props.author}
       </h4>
     </article>
   );
