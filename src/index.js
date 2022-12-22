712ea32aae67d216bbfe0e135a7b3a36e24a7baa
Francis Maina <sebastianfrancix@gmail.com>
2022-12-22 11:50:52 +0300

    created a title component

--- src/index.js
+++ src/index.js
@@ -5,16 +5,31 @@ function BookList() {
   return (
     <section>
       <Book />
-      <Book />
-      <Book />
     </section>
   );
 }
 
 const Book = () => {
-  return <article>This is a book.</article>;
+  return (
+    <article>
+      <Image />
+      <Title />
+    </article>
+  );
 };
 
+const Image = () => {
+  return (
+    <img
+      src="https://m.media-amazon.com/images/W/WEBP_402378-T2/images/I/51VC+Vru96L._SY344_BO1,204,203,200_.jpg"
+      alt="Book image"
+    />
+  );
+};
+
+const Title = () => {
+  return <h1>It Starts with Us: A Novel (It Ends with Us) </h1>;
+};
 export default BookList;
 
 ReactDOM.render(<BookList />, document.getElementById("root"));
