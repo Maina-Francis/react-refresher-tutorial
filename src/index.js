5c7ecc2d2725a867c30dcddfde46fdbe38337a3f
Francis Maina <sebastianfrancix@gmail.com>
2022-12-25 22:23:08 +0300

    added clickHandler function for our button

--- src/index.js
+++ src/index.js
@@ -39,15 +39,23 @@ const Book = (props) => {
   // props destructuring
   const { img, title, author } = props;
 
+  // #clickHandler function for our button
+  const clickHandler = () => {
+    alert("Hello Future Microsoft engineer!");
+  };
+
   return (
     <article className="book">
       <img src={img} alt="" />
-      <h1>{title} </h1>
+      <h1 onClick={() => console.log(title)}>{title} </h1>
       <h4
         style={{ color: "#617d98", fontSize: "0.75rem", marginTop: "0.25rem" }}
       >
         {author}
       </h4>
+      <button type="button" onClick={clickHandler}>
+        Reference Handler
+      </button>
     </article>
   );
 };
